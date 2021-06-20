15ba0b6b6da2729ba9646e32ff3c49cfb8f8a0d0
Sandeep N <sandeepbk01@gmail.com>
2021-06-20 11:45:37 +0530

    tests complited

---
+++ src/contracts/Token.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+  //add minter variable
+  address public minter;
+
+  //add minter changed event
+
+  event MinterChanged(address indexed from , address to);
+
+  constructor() public payable ERC20("Decentralized Bank Currency", "DBC") {
+    //asign initial minter
+    minter = msg.sender;
+  }
+
+  //Add pass minter role function
+
+  function passMinterRole(address dbank) public returns (bool)
+  {
+    require(msg.sender == minter,"Error! msg.sender does not have the required minter role");
+    minter = dbank;
+
+    emit MinterChanged(msg.sender,dbank);
+    return true;
+  }
+
+  function mint(address account, uint256 amount) public {
+    //check if msg.sender have minter role
+
+
+    require(msg.sender == minter,"Error! msg.sender does not have the required minter role");
+		_mint(account, amount);
+	}
+}
